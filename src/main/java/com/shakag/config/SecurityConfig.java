8b2fe133c50e7d56ae5baa2cf335d7c25c8a80c7
skk <654287340@qq.com>
2021-06-17 23:17:40 +0800

    -

--- src/main/java/com/shakag/config/SecurityConfig.java
+++ src/main/java/com/shakag/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.shakag.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @Import(BCryptPasswordEncoder.class)
-public class SecurityConfig {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
