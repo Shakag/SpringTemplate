1358fda5b645c46d0bd92f04b5e693137798c731
Shakag <654287340@qq.com>
2021-07-20 15:46:20 +0800

    2021-07-20 15:46:21

--- src/main/java/com/shakag/controller/SysUserController.java
+++ src/main/java/com/shakag/controller/SysUserController.java
@@ -1,6 +1,12 @@
 package com.shakag.controller;
 
 
+import com.shakag.common.validate.GroupAdd;
+import com.shakag.common.validate.ValidList;
+import com.shakag.entity.SysUser;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -17,5 +23,16 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/sysUser")
 public class SysUserController {
 
+    /**
+     * ValidList<SysUser>:
+     *  ValidList为自定义类，@Valid只能校验JavaBean，而List<E>不是JavaBean所以校验会失败
+     *  既然List不是JavaBean，那我们就把它封装成JavaBean
+     *  ValidList<E>就是封装list后的JavaBean
+     */
+    @PostMapping("/add")
+    public String add(@Validated(GroupAdd.class) @RequestBody ValidList<SysUser> user){
+        System.out.println(user);
+        return "add success";
+    }
 }
 
