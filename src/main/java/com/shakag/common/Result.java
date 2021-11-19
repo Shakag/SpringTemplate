a1cc06a44a7783067966218c4df86094f9e21068
skk <654287340@qq.com>
2021-11-19 11:25:17 +0800

    -

--- src/main/java/com/shakag/common/Result.java
+++ src/main/java/com/shakag/common/Result.java
@@ -1,6 +1,7 @@
 package com.shakag.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,31 +21,38 @@ public class Result<T> implements Serializable {
     public static final Integer DEFAULT_SUCCESS_CODE = 200;
     public static final Integer DEFAULT_ERROR_CODE = 500;
 
+    @ApiModelProperty("返回状态码")
     private int code;
+    @ApiModelProperty("返回提示信息")
     private String msg;
+    @ApiModelProperty("返回数据集")
     private T data;
 
-    public static Result success(){
-        return new Result(DEFAULT_SUCCESS_CODE,DEFAULT_SUCCESS_MESSAGE,null);
+    public static<T> Result<T> success(){
+        return new Result<>(DEFAULT_SUCCESS_CODE,DEFAULT_SUCCESS_MESSAGE,null);
     }
 
-    public static Result success(Object data){
-        return new Result(DEFAULT_SUCCESS_CODE,DEFAULT_SUCCESS_MESSAGE,data);
+    public static<T> Result<T> success(T data){
+        return new Result<>(DEFAULT_SUCCESS_CODE,DEFAULT_SUCCESS_MESSAGE,data);
     }
 
-    public static Result success(String msg,Object data){
-        return new Result(DEFAULT_SUCCESS_CODE,msg,data);
+    public static<T> Result<T> success(String msg,T data){
+        return new Result<>(DEFAULT_SUCCESS_CODE,msg,data);
     }
 
-    public static Result success(int code,String msg,Object data){
-        return new Result(code,msg,data);
+    public static<T> Result<T> success(int code,String msg,T data){
+        return new Result<>(code,msg,data);
     }
 
-    public static Result fail(String msg){
-        return new Result(DEFAULT_ERROR_CODE,msg,null);
+    public static<T> Result<T> fail(){
+        return new Result<>(DEFAULT_ERROR_CODE,DEFAULT_ERROR_MESSAGE,null);
     }
 
-    public static Result fail(int code,String msg){
-        return new Result(code,msg,null);
+    public static<T> Result<T> fail(String msg){
+        return new Result<>(DEFAULT_ERROR_CODE,msg,null);
+    }
+
+    public static<T> Result<T> fail(int code,String msg){
+        return new Result<>(code,msg,null);
     }
 }
