f895f1056de9c88f5a1b744827128e6bd74e7cdc
skk <654287340@qq.com>
2021-11-30 16:43:25 +0800

    2021-11-30 16:43:25

--- src/main/java/com/shakag/TemplateBoot.java
+++ src/main/java/com/shakag/TemplateBoot.java
@@ -1,9 +1,11 @@
 package com.shakag;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.shakag.mapper")
 public class TemplateBoot {
     public static void main(String[] args) {
         SpringApplication.run(TemplateBoot.class,args);
