568ed3c50fe8965cd1db3c8c735d6d0a0560bc35
skk <654287340@qq.com>
2021-12-28 11:14:17 +0800

    2021-12-28 11:14:17

--- src/main/java/com/shakag/mapper/SysUserMapper.java
+++ src/main/java/com/shakag/mapper/SysUserMapper.java
@@ -4,6 +4,8 @@ import com.shakag.entity.SysUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -13,5 +15,5 @@ import org.apache.ibatis.annotations.Mapper;
  * @since 2021-06-10
  */
 public interface SysUserMapper extends BaseMapper<SysUser> {
-
+    public int saveList(List<SysUser> list);
 }
